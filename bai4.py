3fc81483a6ed2097e916ecf41af53c8d9675e8cc
tranducduy-web <tdduy.24174600066@sv.uneti.edu.vn>
2024-10-24 13:39:19 +0700

    bai4

--- bai4.py
+++ bai4.py
@@ -1,1 +1,15 @@
+hieu_dien_the = 220
+cuong_do_dong_dien = 2.7
+gia_dien = 7000
 
+thoi_gian_su_dung = float(input("Thời gian sử dụng bóng điện(s): "))
+
+cong_suat_tieu_thu = hieu_dien_the * cuong_do_dong_dien
+
+thoi_gian_gio = thoi_gian_su_dung / 3600
+dien_nang_tieu_thu = (cong_suat_tieu_thu / 1000) * thoi_gian_gio
+
+tinh_gia_tien = dien_nang_tieu_thu * gia_dien
+
+print("Điện năng tiêu thụ là: ", round(dien_nang_tieu_thu, 2), "kwh")
+print("Giá điện là: ", round(tinh_gia_tien, 2), "Đồng")
